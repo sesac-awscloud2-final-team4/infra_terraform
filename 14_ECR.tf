4d97dc1d32652696edf50bd57d64bdb8ebc57838
jenny <jenny@example.com>
2025-01-20 15:02:44 +0900

    add sg, eks-settings, ecr-settings.

---
+++ 14_ECR.tf
@@ -0,0 +1,13 @@
+resource "aws_ecr_repository" "sesac_ecr_repository" {
+  name                 = "sesac-ecr-repository"  # ECR 리포지토리 이름
+  image_tag_mutability = "MUTABLE"      # 이미지 태그 가변성 설정
+
+  tags = {
+    Name        = "sesac-ecr-repository"
+    Environment = "Production"
+  }
+
+  lifecycle {
+    prevent_destroy = false  # 리포지토리 삭제 가능
+  }
+}

52f86fd2c25ceceb98d34d9336100966170fcc7f
hhch <hhch98@gmail.com>
2025-01-21 19:39:26 +0900

    add solved sg.tf

---
+++ 11_sg.tf
@@ -0,0 +1,32 @@
+resource "aws_security_group" "sesac-security-group-web" {
+  vpc_id = aws_vpc.sesac_vpc.id
+  name = "sesac-security-group-web"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "sesac-security-group-web"
+  }
+}

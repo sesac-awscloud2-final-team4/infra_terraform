c2d17c3eb0ce6b9dc8d053c24185cda4ba876d03
hhch <hhch98@gmail.com>
2025-02-12 16:51:43 +0900

    ADD inbound rule 8080 port at web sequrity

--- 20_sg.tf
+++ 20_sg.tf
@@ -20,6 +20,12 @@ resource "aws_security_group" "sesac_security_group_web" {
     protocol    = "icmp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0

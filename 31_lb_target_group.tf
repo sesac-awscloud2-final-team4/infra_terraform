95b9bb4fb45a2d9f0d63227d377c10fada7607a5
hhch <hhch98@gmail.com>
2025-02-17 19:00:23 +0900

    change alb listerner port 8080 to 80.

--- 31_lb_target_group.tf
+++ 31_lb_target_group.tf
@@ -21,7 +21,7 @@ resource "aws_lb_target_group" "sesac_albtg" {
 
 resource "aws_lb_listener" "sesac_albli" {
   load_balancer_arn = aws_lb.sesac_alb.arn
-  port = 8080
+  port = 80 # 80포트로 설정해야 요청 받을 수 있음
   protocol = "HTTP"
 
   default_action {
